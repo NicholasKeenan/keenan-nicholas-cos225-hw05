e3b0144ef9196c0ea3f65f8151d6bcadb142ae63
NicholasKeenan <nicholas.keenan@maine.edu>
2025-03-28 16:11:48 -0400

    Customer order file

---
+++ CustomerOrder.java
@@ -0,0 +1,44 @@
+public class CustomerOrder {
+
+    private String name;
+    private String date;
+    private int quantity;
+
+    //dfault construct
+    public CustomerOrder() {
+
+        this.name = null;
+        this.date = null;
+        this.quantity = 0;
+
+    }
+
+    //p construct
+    public CustomerOrder ( String name, String date, int quantity ) {
+
+        this.name = name;
+        this.date = date;
+        this.quantity = quantity;
+
+    }
+
+    //fucntion to ship product
+    public void shipProduct() {
+
+        if ( quantity > 0 ) {
+
+            quantity--;
+
+        }
+
+    }
+
+    //tostring
+    @Override
+    public String toString() {
+
+        return String.valueOf ( quantity );
+
+    }
+
+}
